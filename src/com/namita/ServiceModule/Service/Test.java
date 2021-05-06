1befdda60b50577fa47522d3f0a3c9cb284342f4
Namita Rode <namitarode@gmail.com>
2021-05-06 22:24:24 +0530

    Test.java modified

--- src/com/namita/ServiceModule/Service/Test.java
+++ src/com/namita/ServiceModule/Service/Test.java
@@ -3,7 +3,7 @@ package com.namita.ServiceModule.Service;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Testing ModuleDemoApp");
+		System.out.println("In Test Class");
 	}
 
 }
