8a9d602680fd4df75701335967c2c908268b8d74
Namita Rode <namitarode@gmail.com>
2021-05-08 19:30:23 +0530

    Test.java modified in test2 branch

--- src/com/namita/ServiceModule/Service/Test.java
+++ src/com/namita/ServiceModule/Service/Test.java
@@ -3,7 +3,7 @@ package com.namita.ServiceModule.Service;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("In Test Class");
+		System.out.println("In Test Class - Test2 Branch");
 	}
 
 }
