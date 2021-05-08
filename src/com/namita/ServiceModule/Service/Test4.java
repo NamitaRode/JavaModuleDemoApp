a6d31de8d9b2b2c403e6755eea5cd422fdb735e8
Namita Rode <namitarode@gmail.com>
2021-05-08 11:54:41 +0530

    Class Test4 modified - new sysout added , first sysout modified

--- src/com/namita/ServiceModule/Service/Test4.java
+++ src/com/namita/ServiceModule/Service/Test4.java
@@ -3,7 +3,8 @@ package com.namita.ServiceModule.Service;
 public class Test4 {
 
 	public static void main(String[] args) {
-		System.out.println("In Test4 Class");
+		System.out.println("In Test4 Class - modified for rebase demo");
+		System.out.println("new sysout added");
 	}
 
 }
