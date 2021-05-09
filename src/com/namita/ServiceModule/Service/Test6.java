ffa3ad52cbf77ad41728278c7fe1dec11771153b
Namita Rode <namitarode@gmail.com>
2021-05-09 22:56:45 +0530

    Test6 removed

--- src/com/namita/ServiceModule/Service/Test6.java
+++
@@ -1,9 +0,0 @@
-package com.namita.ServiceModule.Service;
-
-public class Test6 {
-
-	public static void main(String[] args) {
-		System.out.println("in Class Test6 --testing reset option");
-	}
-
-}
