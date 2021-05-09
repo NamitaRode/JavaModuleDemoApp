7b515f2e82ff7efaf29b96e79cb37ecd1b01d8e2
Namita Rode <namitarode@gmail.com>
2021-05-09 22:38:56 +0530

    Test6 modified

--- src/com/namita/ServiceModule/Service/Test6.java
+++ src/com/namita/ServiceModule/Service/Test6.java
@@ -3,8 +3,7 @@ package com.namita.ServiceModule.Service;
 public class Test6 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("in Class Test6 --testing reset option");
 	}
 
 }
